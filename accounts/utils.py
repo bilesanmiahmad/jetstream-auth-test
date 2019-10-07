7fefa602f0bc490c1287620fd7cc1be4e9746685
Bilesanmi Ahmad <fbilesanmi@gmail.com>
2019-10-07 15:17:45 +0000

    initial user and profile models

---
+++ accounts/utils.py
@@ -0,0 +1,5 @@
+
+def get_fb_auth_code():
+    FB_APP_ID = '2562093850479340'
+    FB_REDIRECT_URI = 'https://www.jetstreamafrica.com/'
+    https://www.facebook.com/v4.0/dialog/oauth?client_id=2562093850479340&redirect_uri="https://www.agritradingzone.com/"&state="{st=state123abc,ds=123456789}"
