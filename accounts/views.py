bdbc73ec0886fccd4bfea0be20cea1c5295bc41b
Bilesanmi Ahmad <fbilesanmi@gmail.com>
2019-10-07 16:16:21 +0000

    added permissions and serializers

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from accounts.models import User
+from accounts.serializers import UserSerializer
+from accounts.permissions import IsLoggedInUserOrAdmin, IsAdminUser
 
 # Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_permissions(self):
+        permission_classes = []
+        if self.action == 'create':
+            permission_classes = [AllowAny]
+        elif self.action == 'retrieve' or self.action == 'update' or self.action == 'partial_update':
+            permission_classes = [IsLoggedInUserOrAdmin]
+        elif self.action == 'list' or self.action == 'destroy':
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
